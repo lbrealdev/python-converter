e258de70bf5630b690afbed3ce5db4a344aeea93
lbrealdev <lbrealdeveloper@gmail.com>
2024-06-17 23:30:33 +0200

    feat: refactor script

--- converter.py
+++ converter.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-import pypandoc
+# import pypandoc
 
 
 if len(sys.argv) < 2:
@@ -22,20 +22,33 @@ markdown_files = list(SOURCE_PATH_TO_MD.rglob("*.md"))
 if not markdown_files:
     print(f"No markdown files found in {SOURCE_PATH_TO_MD}")
 
+source_md_files = []
+output_pdf_files = []
+
 print("Convert MD to PDF:\n")
 for markdown_file in markdown_files:
     relative_path = markdown_file.relative_to(SOURCE_PATH_TO_MD)
-    pdf_output_path = DESTINATION_PATH_TO_PDF / relative_path.with_suffix('.pdf')
-    
+    pdf_output_path = DESTINATION_PATH_TO_PDF / relative_path.with_suffix(".pdf")
+
     try:
-        #pypandoc.convert_file(
-        #    str(markdown_file), 
-        #    'pdf', 
-        #    outputfile=str(pdf_file), 
+        # pypandoc.convert_file(
+        #    str(markdown_file),
+        #    'pdf',
+        #    outputfile=str(pdf_file),
         #    extra_args=['--pdf-engine=pdflatex']
-        #)
+        # )
         pdf_output_path.parent.mkdir(parents=True, exist_ok=True)
         pdf_output_path.touch(exist_ok=True)
-        print(f"{markdown_file} -> {pdf_output_path}")
+
+        source_md_files.append(markdown_file)
+        output_pdf_files.append(pdf_output_path)
     except Exception as e:
         print(f"Error converting {markdown_file}: {e}")
+
+print("Source markdown files:")
+for input_md in source_md_files:
+    print(input_md)
+
+print("\nOutput PDF files:")
+for output_pdf in output_pdf_files:
+    print(output_pdf)
