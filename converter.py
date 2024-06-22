4fe04ef59119ac5786b9d38edc227240b8a1e6ec
lbrealdev <lbrealdeveloper@gmail.com>
2024-06-22 21:37:41 +0200

    feat: convert_pandoc function and ruff format

--- converter.py
+++ converter.py
@@ -11,26 +11,46 @@ if len(sys.argv) < 2:
     print("Usage: python converter.py <path-to-markdown>")
     sys.exit(1)
 
-# install_pandoc function will run
+
+# install_pandoc function
 def install_pandoc():
     pandoc_bin = shutil.which("pandoc")
     if not pandoc_bin:
         from pypandoc.pandoc_download import download_pandoc
-        
+
         print("Pandoc binary was not found!")
-        
-        # Set the environment variable PYPANDOC_PANDOC 
+
+        # Set the environment variable PYPANDOC_PANDOC
         # to the only location where pandoc will be searched
         os.environ.setdefault("PYPANDOC_PANDOC", "/usr/bin/pandoc")
 
         # Pandoc binary installation and download directory.
-        # /usr/bin & /tmp
         bin_dir = Path(os.getenv("PYPANDOC_PANDOC")).parent
         tmp_dir = Path(bin_dir.root) / "tmp"
-        
+
         print(f"Downloading pandoc to {tmp_dir} and installing it in {bin_dir} ...")
         download_pandoc(targetfolder=str(bin_dir), download_folder=str(tmp_dir))
 
+
+# convert_pandoc function
+def convert_pandoc(input, output, auth: bool = False):
+    markdown = str(input)
+    pdf = str(output)
+
+    args = [
+        "--pdf-engine=pdflatex",
+        "--from=markdown-implicit_figures+rebase_relative_paths",
+        "--extract-media=.",
+    ]
+
+    if auth:
+        args.append(f"--request-header=Authorization: token {GITHUB_AUTH_TOKEN}")
+
+    pypandoc.convert_file(markdown, "pdf", outputfile=pdf, extra_args=args)
+
+
+GITHUB_AUTH_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_AUTH = bool(GITHUB_AUTH_TOKEN)
 
 # Path to the directory where the markdown and
 # destination path that will be created '_output' for converted pdf files.
@@ -41,11 +61,6 @@ DESTINATION_PATH_TO_PDF = Path(SOURCE_PATH_TO_MD) / "_output"
 # This variable must be set if the markdown files you want
 # convert to PDF contain images with URL image referencing an
 # image in a private repository.
-GITHUB_AUTH_TOKEN = os.getenv("GITHUB_TOKEN")
-
-if not GITHUB_AUTH_TOKEN or GITHUB_AUTH_TOKEN is None:
-    print("Github authentication token is not set.")
-    sys.exit(1)
 
 if not SOURCE_PATH_TO_MD.exists() or not SOURCE_PATH_TO_MD.is_dir():
     print("Invalid path to markdown directory!")
@@ -78,17 +93,7 @@ for markdown_file in markdown_files:
 
     try:
         install_pandoc()
-        pypandoc.convert_file(
-            str(markdown_file),
-            "pdf",
-            outputfile=str(pdf_output_path),
-            extra_args=[
-                "--pdf-engine=pdflatex",
-                "--from=markdown-implicit_figures+rebase_relative_paths",
-                f"--request-header=Authorization: token {GITHUB_AUTH_TOKEN}",
-                "--extract-media=.",
-            ],
-        )
+        convert_pandoc(input=markdown_file, output=pdf_output_path, auth=GITHUB_AUTH)
     except Exception as e:
         print(f"Error converting {markdown_file.absolute()}: {e}")
         sys.exit(1)
